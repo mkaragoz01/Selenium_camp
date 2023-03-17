2abae7c894ba559f43b9ef34afc44c827ba469c1
Muhammet Mustafa Karagöz <karagozmuhammet45@gmail.com>
2023-03-18 00:19:47 +0300

    first commit

---
+++ gün1_ödev1.py
@@ -0,0 +1,9 @@
+string_nedir = "String metinsel veri tipidir."
+integer_nedir = int(5) # Tam sayılardan oluşan veri tipidir.
+float_nedir = float(2.5) #küsüratlı sayılardan oluşan veri tipidir.
+dictionary_nedir = {"apple": "elma"} # key ve value adındaki iki değişkenden oluşan veri tipi.
+list_nedir = [1,2,3,4,5] # sıralanabilen ve değiştirilebilen veri tipi.
+tuple_nedir = (12,3,4,5) # liste veri tipinin değiştirilemez halidir.
+
+# Notlar kısmına eklenen metinler bir değişkene atanmış olabilir.
+# Şuan ki ders bitmeden sonraki dersin ödevlerine bakmaya çalışırsak şart bloğu devreye girer ve buna izin vermez
---
+++ gün2_ödev1.py
@@ -0,0 +1,74 @@
+
+isim = input("Lütfen isminizi giriniz: ")
+soy_isim = input("Lütfen soy isminizi giriniz: ")
+
+liste = []
+
+
+
+# Aldığı isim soy isim ile listeye öğrenci ekleyen
+def append_liste(isim,soy_isim):
+    liste.append(isim+" "+soy_isim)
+
+append_liste(isim,soy_isim)
+print(liste)
+
+
+
+# Aldığı isim soy isim ile eşleşen değeri listeden kaldıran
+def remove_liste(isim,soy_isim):
+    liste.remove(isim+" "+soy_isim)
+
+remove_liste(isim,soy_isim)
+print(liste)
+
+
+
+# Listeye birden fazla öğrenci eklemeyi mümkün kılan
+def add_more():
+    while True:
+        isim = input("Lütfen isminizi giriniz (ekleme tamamsa eğer 0 a basın!!!): ")
+        
+        if isim == "0":
+            break
+        
+        soy_isim = input("Lütfen soy isminizi giriniz: ")
+        append_liste(isim,soy_isim)
+
+add_more()
+print(liste)
+
+
+
+# Öğrencinin listedeki index numarası öğrenci numarası olarak kabul edildiğini düşünerek öğrencinin numarasını öğrenmeyi mümkün kılan
+
+isim = input("Lütfen isminizi giriniz: ")
+soy_isim = input("Lütfen soy isminizi giriniz: ")
+liste.append(isim+" "+soy_isim)
+
+def student_num(isim,soy_isim):
+    for j in range(len(liste)):
+        if liste[j] == isim+" "+soy_isim:
+            return j
+
+num = student_num(isim,soy_isim)
+print(f"Öğrenci numarası: {num+1}") # indisler 0 dan başladığı için +1 ekledim.
+
+
+
+# Listeden birden fazla öğrenci silmeyi mümkün kılan (döngü kullanınız)
+def remove_moore():
+    for i in liste:
+        print(liste)
+        
+        isim = input("Silmek istediğiniz kişinin ismini giriniz(çıkış yapmak için 0 giriniz!!!): ")
+        if isim == "0":
+            break
+        soy_isim = input("Silmek istediğiniz kişinin soy ismini giriniz: ")
+        
+        liste.remove(isim+" "+soy_isim)
+
+remove_moore()
+print(liste)
+        
+        
